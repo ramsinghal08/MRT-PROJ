17f251e1eb9a0a539a783fba5ce6044c0ed13e7b
ramsinghal08 <singhalram44@gmail.com>
2025-12-09 21:12:41 +0530

    added final "see" function and also shelves integration

--- src/mapping/mapping/mapping_shi.py
+++ src/mapping/mapping/mapping_shi.py
@@ -8,6 +8,7 @@ class GeneratedMap: #generated  by bots, shared by all of them
         self.parent = parent
         self.grid = {}
         self.Frontier = {}
+        self.shelves = {}
         for x in range(0,map_size):
             for y in range(0,map_size):
                 self.grid[(x,y)] = 3 #unexplored
@@ -61,6 +62,44 @@ class GeneratedMap: #generated  by bots, shared by all of them
                     self.Frontier[(x, y)] = id # marking which bot found this frontier
                     break
 
+    def identify_shelves(self):
+        shelves = {}
+        visited = set()
+        current_id = 1
+        known_coords = list(self.grid.keys())
+
+        for (x, y) in known_coords:
+            val = self.get(x, y)
+            if val == 1 and (x, y) not in visited and 0<x<60 and 0<y<60 :
+                group = []
+                queue = [(x, y)]
+                visited.add((x, y))
+                while queue:
+                    cx, cy = queue.pop(0)
+                    group.append((cx, cy))
+                    for nx, ny in self.neighbors(cx, cy):
+                        if self.get(nx, ny) == 1 and (nx, ny) not in visited:
+                            visited.add((nx, ny))
+                            queue.append((nx, ny))
+
+                xs = [p[0] for p in group]
+                ys = [p[1] for p in group]
+                min_x, max_x = min(xs), max(xs)
+                min_y, max_y = min(ys), max(ys)
+                
+                if (min_x == 0 or max_x == 59 or min_y == 0 or max_y == 59):
+                    continue
+                shelf_x = max_x
+                shelf_y = max_y
+                for i in range(min_x, max_x + 1):
+                    for j in range(min_y, max_y + 1):
+                        self.setValue(i, j, 4)
+                        visited.add((i, j))
+
+                shelves[current_id] = (shelf_x, shelf_y)
+                current_id += 1
+        self.shelves = shelves
+    
     def printmap(self):
         # render the known explored region __ is so python treats it as a special function and prints a string when we code print(str)
         b = self.bounds()
